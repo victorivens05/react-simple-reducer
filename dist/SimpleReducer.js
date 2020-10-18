f8c6b228f67d3a94992f29cc614c227d0b075520
Victor Ivens Pereira <victorivens05@gmail.com>
2020-10-18 17:41:45 -0300

    Init and connect to redux devtools (#1)

---
+++ dist/SimpleReducer.js
@@ -0,0 +1,72 @@
+import React, { useReducer, createContext, useContext, useMemo, useRef, useEffect } from 'react';
+import produce from 'immer';
+
+function createSimpleStore(initialState, actionsMap, asyncActionsMap) {
+  const StateContext = createContext(null);
+  const DispatchContext = createContext(null);
+
+  const reducer = (state, action) => {
+    const {
+      type,
+      payload
+    } = action;
+    const nextState = produce(state, draftState => actionsMap[type](draftState, payload));
+    return nextState;
+  };
+
+  const Provider = ({
+    children
+  }) => {
+    const [reducerState, reducerDispatch] = useReducer(reducer, initialState);
+    const reducerStateRef = useRef(reducerState);
+    useEffect(() => {
+      reducerStateRef.current = reducerState;
+    }, [reducerState]);
+
+    const DispatchWrapper = action => {
+      if (typeof action === 'function') return action(reducerDispatch, () => reducerStateRef.current);
+      return reducerDispatch(action);
+    };
+
+    return /*#__PURE__*/React.createElement(StateContext.Provider, {
+      value: reducerState
+    }, /*#__PURE__*/React.createElement(DispatchContext.Provider, {
+      value: DispatchWrapper
+    }, children));
+  };
+
+  const GetState = ({
+    children
+  }) => {
+    const s = useState();
+    return children(s);
+  };
+
+  const useState = () => useContext(StateContext);
+
+  const useDispatch = () => useContext(DispatchContext);
+
+  const actions = _createActionsFromActionMap(actionsMap);
+
+  return {
+    useState,
+    useDispatch,
+    Provider,
+    GetState,
+    thunks: asyncActionsMap,
+    actions: actions
+  };
+}
+
+function _createActionsFromActionMap(actionsMap) {
+  const actions = {};
+  Object.keys(actionsMap).map(k => {
+    actions[k] = payload => ({
+      type: k,
+      payload
+    });
+  });
+  return actions;
+}
+
+export { createSimpleStore };
--- src/Example.tsx
+++ src/Example.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { createSimpleStore } from './SimpleReducer'
+import { createSelector } from 'reselect'
 
 const initialState = {
   loading: false,
   user: {
     name: 'Victor' as string | null,
     email: null,
-  }
+  },
+  random: 0,
+  lines: [] as { lineId: number, lineText: string }[],
 }
 const AuthStore = createSimpleStore(initialState, {
+  randomize (state) {
+    state.random = Math.random()
+  },
   clear () {
     return {
       ...initialState,
@@ -23,7 +29,7 @@ const AuthStore = createSimpleStore(initialState, {
     state.user.email = payload.userEmail
     state.loading = false
   },
-  loggout: (state, notify: boolean) => {
+  loggout: (state) => {
     state.user.name = null
     state.user.email = null
     state.loading = false
@@ -32,52 +38,188 @@ const AuthStore = createSimpleStore(initialState, {
     state.loading = false
   },
   other (state) {
+  },
+  toggleLoading (state) {
+    state.loading = !state.loading
+  },
+  init (state) {
+    state.random = 69420
+  },
+  addLine (state) {
+    state.lines.push({ lineId: state.lines.length + 1, lineText: 'Victor' })
   }
 }, {
   login (usuario: string, senha: number) {
-    return async (actions, getState) => {
+    return async (dispatch, getState) => {
       const userName = usuario
       const userEmail = senha
-      actions.loginStarted()
+      dispatch({ type: 'loginStarted' })
       console.log('getState 1', getState())
       setTimeout(() => {
         console.log('getState 2', getState())
-        actions.logginSuccess({ userName, userEmail })
+        dispatch({ type: 'logginSuccess', payload: { userEmail, userName } })
         console.log('getState 3', getState())
       }, 2000)
+    }
+  },
+  init () {
+    return async (dispatch) => {
+      setTimeout(() => {
+        dispatch(AuthStore.actions.randomize())
+      }, 2000)
+      setTimeout(() => {
+        dispatch(AuthStore.actions.randomize())
+      }, 3000)
     }
   }
 })
 
-const Child = () => {
-  const state = AuthStore.useState()
-  const actions = AuthStore.useActions()
-  const asyncActions = AuthStore.useAsyncActions()
-  console.log({ state: state })
-  return (
-    <>
-      {state.user.name}
-      <button onClick={() => actions.loggout(false)} >logout</button>
-      <button onClick={() => actions.logginSuccess({ userName: Math.random(), userEmail: 'vito@ivens' })} >change login info</button>
-      <button onClick={() => asyncActions.login('login', Math.random())} >login async</button>
-      <button onClick={() => actions.clear()} >CLEAR</button>
-    </>
-  )
-}
+type IState = ReturnType<typeof AuthStore.useState>
+const s1 = createSelector(
+  (s: IState) => s.user,
+  (s: IState) => s.loading,
+  (user, loading) => {
+    console.log('s1')
+    return { USUARIO: user, LOADING: loading }
+  }
+)
+
+const s2 = createSelector(
+  (s: IState) => s.user,
+  (user) => {
+    console.log('s2')
+    return user
+  }
+)
+
+const s3 = createSelector(
+  (s: IState & { id }) => s.user,
+  ({ id }) => id,
+  (user, id) => {
+    console.log('s3')
+    console.log({ id })
+    return id
+  }
+)
 
 const SimpleReducerTest = () => {
+  function init (dispatch: ReturnType<typeof AuthStore.useDispatch>) {
+    dispatch(AuthStore.actions.init())
+  }
   return (
     <>
-      <AuthStore.Provider>
+      <AuthStore.Provider init={init}>
         <AuthStore.GetState>
           {state => (
             <div>{state.user.email}</div>
           )}
         </AuthStore.GetState>
         <Child />
+        <Lines />
       </AuthStore.Provider>
     </>
   )
 }
 
+const Child = () => {
+  const state = AuthStore.useState()
+  const dispatch = AuthStore.useDispatch()
+  const _s1 = s1(state)
+  const _s2 = s2(state)
+  const [id, setId] = useState('')
+  const [codigo, setCodigo] = useState('')
+  const stateWithId = useMemo(() => ({ ...state, id }), [id, state])
+  const _s3 = s3(stateWithId)
+
+  return (
+    <>
+      <input value={id} onChange={e => setId(e.target.value)} />
+      <input value={codigo} onChange={e => setCodigo(e.target.value)} />
+      loading: {'' + state.loading}<br />
+      {state.user.name}<br />
+      {state.random}<br />
+      <button onClick={() => dispatch({ type: 'loggout' })}> logout</button>
+      <button onClick={() => dispatch({
+        type: 'logginSuccess',
+        payload: { userEmail: Math.random(), userName: 'vito@ivens' }
+      })}>change login info</button>
+      <button onClick={() => dispatch(AuthStore.actions.logginSuccess({
+        userEmail: Math.random(),
+        userName: 'action',
+      }))}>change login info through action</button>
+      <button onClick={() => dispatch(AuthStore.thunks.login('login', Math.random()))} >login async</button>
+      <button onClick={() => dispatch(AuthStore.actions.clear())} >CLEAR</button>
+      <button onClick={() => dispatch(AuthStore.actions.randomize())}>randomize</button>
+      <button onClick={() => dispatch(AuthStore.actions.toggleLoading())}>toggle loading</button>
+      <button onClick={() => dispatch(AuthStore.actions.addLine())}>add line</button>
+    </>
+  )
+}
+
+const Lines = () => {
+  const { lines } = AuthStore.useState()
+  return (
+    <div>
+      {
+        lines.map(l => (
+          <div style={{ borderBottom: '1px solid #ccc' }} key={l.lineId}>{l.lineId} - {l.lineText}</div>
+        ))
+      }
+    </div >
+  )
+}
+
 export { SimpleReducerTest }
+
+
+// const TodosStore = createSimpleStore({
+//   todo: '',
+//   todos: [] as TodoType[],
+// }, {
+//   updateTodo (state, todo) {
+//     state.todo = todo
+//   },
+//   addTodo (state) {
+//     state.todos.push({ todo: state.todo, saved: false })
+//   },
+//   saveTodosSuccess (state) {
+//     state.todos.forEach(t => t.saved = true)
+//   },
+// }, {
+//   saveTodos ({ saveAll = true }) {
+//     return async (dispatch, getState) => {
+//       const todos = getState().todos
+//       await api.save(todos)
+//       dispatch(TodosStore.actions.saveTodosSuccess())
+//     }
+//   }
+// })
+
+// const TodoComponent = () => {
+//   return (
+//     <TodosStore.Provider>
+//       <NewTodo />
+//       <TodoList />
+//       <SaveTodos />
+//       <TodosStore.GetState>
+//         {state => (<>Total of Todos: {state.todos.length}</>)}
+//       </TodosStore.GetState>
+//     </TodosStore.Provider>
+//   )
+// }
+
+// const ChildComponent = () => {
+//   // get anything from the state using useState()
+//   const { todo, todos } = TodosStore.useState()
+
+//   // get the dispatch function to use actions and async actions
+//   const dispatch = TodosStore.useDispatch()
+
+//   // dispatch actions using two methods: object and actions
+//   dispatch({ type: 'updateTodo', payload: 'NEW TODO TEXT' })
+//   dispatch(TodosStore.actions.updateTodo('NEW TODO TEXT'))
+
+//   // dispatch async actions using thunks
+//   dispatch(TodosStore.thunks.saveTodos({ saveAll: false }))
+
+// }
